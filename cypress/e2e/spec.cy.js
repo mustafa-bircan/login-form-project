683bffd748794a50fdcbc3346a52d19cad941688
mustafa-bircan <msbrtfcn@gmail.com>
2025-01-23 21:42:18 +0300

    Başarılı form submit edildiğinde ve Geçersiz e-posta girildiğinde hata mesaj gösteriliyor mu testleri eklendi.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,27 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+import { should } from "chai";
+
+describe('Login Form', () => {
+    it('Başarılı form doldurulduğunda submit edebiliyorum.', () => {
+      cy.visit('http://localhost:5177/');
+
+      cy.get('[data-cy="email-input"]').type('erdem.guntay@wit.com.tr'); 
+      cy.get('[data-cy="password-input"]').type('9fxIH0GXesEwH_I');
+      cy.get('[data-cy="check-checkbox"]').check();
+      cy.get('[data-cy="submit-button"]').should('not.be.disabled').click(); 
+
+      cy.url().should('include','/success');
+    });
+
+    it('Geçersiz e-posta adresi girildiğinde hata mesajı gösterilmeli.', () => {
+      cy.visit('http://localhost:5177/');
+
+      cy.get('[data-cy="email-input"]').type('erdem.guntay@wit')
+      cy.get('[data-cy="submit-button"]').should('be.disabled');
+      cy.get('[data-cy="error-message"]').should('contain.text','Please enter a valid email address');
+    });
+
+
+    
+});
+
+    
